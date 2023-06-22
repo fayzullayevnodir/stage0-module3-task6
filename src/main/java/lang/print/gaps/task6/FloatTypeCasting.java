097c43578d568cb85119dd4e7f7c26c7ee3b8bca
nodirbekcs <fayzullayevnodir0@gmail.com>
2023-06-22 18:47:36 +0500

    great

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task6;
 
+import java.sql.SQLOutput;
+
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        if (numberToBeRounded%10>=9.5){
+            System.out.println((int)numberToBeRounded+1);
+        }else{
+
+        System.out.println((int)numberToBeRounded);}
     }
 }
